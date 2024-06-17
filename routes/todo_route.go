fb6748b24e43031564ae60caaf98155560f13bdb
Ferhat <erogluferhat@outlook.com>
2024-06-17 22:16:46 +0300

    Add TodoRoute with CRUD endpoints for todos

---
+++ routes/todo_route.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/ferhateroglu/go-fiber/controllers"
+	"github.com/ferhateroglu/go-fiber/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+type TodoRouter struct {
+	todoController *controllers.TodoController
+}
+
+func NewTodoRouter(todoController *controllers.TodoController) *TodoRouter {
+	return &TodoRouter{
+		todoController: todoController,
+	}
+}
+
+func (tr *TodoRouter) SetupRoutes(app *fiber.App) {
+	todoGroup := app.Group("/api/todos")
+
+	todoGroup.Get("/", tr.todoController.GetAllTodos)
+	todoGroup.Post("/", middlewares.ValidateRequest(middlewares.TodoRequest{}), tr.todoController.CreateTodo)
+	todoGroup.Get("/:id", middlewares.ValidateRequest(middlewares.GetTodoRequest{}), tr.todoController.GetTodoById)
+	todoGroup.Put("/:id", middlewares.ValidateRequest(middlewares.UpdateTodoRequest{}), tr.todoController.UpdateTodo)
+	todoGroup.Delete("/:id", middlewares.ValidateRequest(middlewares.DeleteTodoRequest{}), tr.todoController.DeleteTodo)
+}
